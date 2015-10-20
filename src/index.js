2bdddd66980d931d367601669aca2f6c6e6843cc
Niklas Walter <walter.niklas@gmail.com>
2015-10-20 22:33:19 +0200

    Updated package to support angular browserify integration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Should already be required, here for clarity
+require('angular');
+
+// Now load auth interceptor module.
+require('./http-auth-interceptor');
+
+// Export namespace
+module.exports = 'http-auth-interceptor';
